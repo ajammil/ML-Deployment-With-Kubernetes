bdbc5be504fbf03ce0177d0f2be52d761fdd7481
AbdElRahman ElGamil <abdurrumanmohamed@gmail.com>
2020-06-11 15:46:44 +0200

    README updated

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -9,11 +9,11 @@ dockerpath=ajammil/prediction_app:latest
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run prediction_app --image=$dockerpath --port=80
+kubectl run predictionapp --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
 kubectl get pods --all-namespaces
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward prediction_app 8000:80
+kubectl port-forward predictionapp-6f7c5bbfc4-wdc6k  8000:80
